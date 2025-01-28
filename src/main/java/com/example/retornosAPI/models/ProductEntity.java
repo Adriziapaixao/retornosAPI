546fe619e9ac5ddd43bf77e4ebd473e1f1482f9b
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-01-28 11:21:31 -0300

    feat: atualiza a classe ProductEntity com os requisitos de descrição, quantidade e categoria

--- src/main/java/com/example/retornosAPI/models/ProductEntity.java
+++ src/main/java/com/example/retornosAPI/models/ProductEntity.java
@@ -11,41 +11,77 @@ public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    private String description;
+
     private Double price;
+
+    private int quantity;
+
+    private String category;
 
     public ProductEntity() {
     }
 
-    public ProductEntity(Long id, String name, Double price) {
+    public ProductEntity(Long id, String name, String description, Double price, int quantity, String category) {
         this.id = id;
         this.name = name;
+        this.description = description;
         this.price = price;
+        this.quantity = quantity;
+        this.category = category;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
+    public ProductEntity(Object o, String name, Double price) {
     }
 
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Double getPrice() {
         return price;
     }
 
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
